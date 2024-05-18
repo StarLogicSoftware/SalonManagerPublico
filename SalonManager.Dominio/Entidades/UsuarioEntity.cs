c141aa8c06919276b5c036fe038de0c66abb6d50
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-05-17 21:18:55 -0300

    commit

--- SalonManager.Dominio/Entidades/UsuarioEntity.cs
+++ SalonManager.Dominio/Entidades/UsuarioEntity.cs
@@ -17,7 +17,7 @@ namespace SalonManager.Dominio.Entidades
         [MinLength(8)]
         public string Contraseña { get; set; }
 
-        public int RolId { get; set; } // comentario desde repositorios
+        public int RolId { get; set; } // comentario desde repositorios 2
         // un comentario
 
         public RolEntity Rol { get; set; }
